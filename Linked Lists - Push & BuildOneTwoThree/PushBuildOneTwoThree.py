4985a3ef90ab1f34c67ac0fe84bd424223b369d4
Soph <tsyuksofija@gmail.com>
2026-03-24 11:53:14 +0200

    Add linked-list push and build_one_two_three
    Implement push(head, data) to prepend a Node and build_one_two_three() to construct a 1->2->3 linked list. Also add tests_passed.png indicating the tests ran successfully.

---
+++ Linked Lists - Push & BuildOneTwoThree/PushBuildOneTwoThree.py
@@ -0,0 +1,22 @@
+from preloaded import Node
+
+'''
+Node is defined in preloaded like this:
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+def push(head, data):
+    dat = Node(data)
+    dat.next = head
+    return dat
+
+def build_one_two_three():
+    chained = None
+    chained = push(chained, 3)
+    chained = push(chained, 2)
+    chained = push(chained, 1)
+    return chained
